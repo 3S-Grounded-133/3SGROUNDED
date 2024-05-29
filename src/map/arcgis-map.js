9ecdb106d73e3b74408e6380c79f27e99394e18b
OdiousFox <goombajames@yahoo.com>
2024-05-29 11:13:00 +0200

    V6: Updated to live

---
+++ src/map/arcgis-map.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import "./arcgis-map.css";
+
+const ArcgisMap = () => {
+    const [appUrl, setAppUrl] = useState("https://experience.arcgis.com/experience/e0855e0eeb8d4e4abd4e742e8f6a922a/"); // default map ID
+
+    return (
+        <div className="map-container">
+            <iframe
+                src={appUrl}
+                width="100%"
+                height="100%"
+                frameBorder="0"
+                allowFullScreen
+                title="ArcGIS Experience Builder"
+            ></iframe>
+        </div>
+    );
+};
+
+export default ArcgisMap;
